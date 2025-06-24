eef0a13c619bf2f182485c90d7717ab1b3a5994a
tmi-t <wkdrndydtlr@naver.com>
2025-06-24 17:20:30 +0900

    [fix] memberTest 파일 명 수정 및 디렉토리 위치 수정 (#78)
    * refactor: 이슈 템플릿 수 정
    
    * refactor: 이슈 템플릿 수정
    
    * feat: 블랭크 이슈 템플릿 비활성화
    
    * feat: 지라 이슈 생성 yml 파일 추가
    
    * feat: 지라 이슈 닫기 yml 파일 추가
    
    * refactor: 지라 이슈 생성 yml 파일 수정
    
    * Update issue_template.yml
    
    * Rename issue_template.yml to issue_form.yml
    
    * Rename issue_form.yml to issue-form.yml
    
    * Update create-jira-issue.yml
    
    * Update create-jira-issue.yml
    
    * Update create-jira-issue.yml
    
    * Update issue-form.yml
    
    * Update close-jira-issue.yml
    
    * (fix) before pull
    
    * (fix) 동행 관련 프로필 기능 member 도메인으로 이동
    
    * (feat) member test코드 작성
    
    * (fix) memberTest 파일 삭제 및 이름 변경
    
    ---------
    
    Co-authored-by: JonghyeokNam <109861353+JonghyeokNam@users.noreply.github.com>

--- src/test/java/com/arom/with_travel/domain/member/service/MemberProfileTest.java
+++ src/test/java/com/arom/with_travel/domain/member/service/MemberProfileTest.java
@@ -1,11 +1,11 @@
 package com.arom.with_travel.domain.member.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@DataJpaTest
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SuppressWarnings("NonAsciiCharacters")
