55c6c2b8edd3f9ed7f3b7dab61f77fc352e3bc1a
JonghyeokNam <njh9750@naver.com>
2025-06-24 14:39:34 +0900

    Survey 도메인, Repository, Service 테스트 코드 작성 (#56)

--- src/main/java/com/arom/with_travel/domain/survey/dto/request/SurveyRequestDto.java
+++ src/main/java/com/arom/with_travel/domain/survey/dto/request/SurveyRequestDto.java
@@ -2,6 +2,8 @@ package com.arom.with_travel.domain.survey.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +11,8 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor   // <— 모든 필드를 초기화하는 생성자 추가
+@Builder
 public class SurveyRequestDto {
 
     @NotBlank(message = "질문은 비어 있을 수 없습니다.")
