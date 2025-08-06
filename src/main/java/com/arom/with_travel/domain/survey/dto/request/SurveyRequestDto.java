9698bcf75f2d74b6b5f60d61d39e8b6bdd9a7f89
iamseojin <jsj05300@naver.com>
2025-08-06 17:30:22 +0900

    [refactor] 설문(Survey) 엔티티에서 question 필드 제거 및 관련 로직 정리

--- src/main/java/com/arom/with_travel/domain/survey/dto/request/SurveyRequestDto.java
+++ src/main/java/com/arom/with_travel/domain/survey/dto/request/SurveyRequestDto.java
@@ -15,9 +15,6 @@ import java.util.List;
 @Builder
 public class SurveyRequestDto {
 
-    @NotBlank(message = "질문은 비어 있을 수 없습니다.")
-    private String question;
-
     @NotEmpty(message = "설문 답변은 최소 1개 이상이어야 합니다.")
     private List<String> answers;
 }
